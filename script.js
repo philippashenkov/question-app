ae19dab4e6f137e8bdea28d8154815abe12bf52c
Philip <reikoslv@gmail.com>
2020-11-28 15:30:48 +0200

    basic build

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is ...',
+        a_1: '10',
+        a_2: '19',
+        a_3: '26',
+        a_4: '110',
+        correct: 'c'
+    },{
+        question: 'What is the most used programming language in 2020?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JS',
+        correct: 'd'
+    }, {
+        question: 'Who is the President of US',
+        a: '...',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    }, {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopter Terminals MotorBoats',
+        correct: 'a'
+    }, {
+        question: 'What year was JS launched?',
+        a: '2020',
+        b: '2019',
+        c: '2018',
+        s: 'none of the above',
+        correct: 'd '
+    }
+]
